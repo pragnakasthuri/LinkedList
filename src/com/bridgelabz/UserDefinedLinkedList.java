9253f0373615b00a5988908086983b572964d112
pragnakasthruri <kpragna25@gmail.com>
2022-03-02 18:46:09 +0530

    Ability to create linked list to add 30 and 56 to 70

--- src/com/bridgelabz/UserDefinedLinkedList.java
+++ src/com/bridgelabz/UserDefinedLinkedList.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 /**
- * Create a simple Linked List of 56, 30 and 70
+ * Ability to create Linked List by adding 30 and 56 to 70
  */
 
 public class UserDefinedLinkedList {
@@ -12,23 +12,24 @@ public class UserDefinedLinkedList {
      * Traverse through the node and prints the data
      */
     public void display() {
-        Node node = this.head;
+        Node node = this.tail;
+        System.out.print("Linked List Sequence: ");
         while (node != null) {
-            System.out.print(node.getData() + " \n");
+            System.out.print(node.getData() + (node.getNext() == null ? "" : "->"));
             node = node.getNext();
         }
     }
 
     public static void main(String[] args) {
         UserDefinedLinkedList userDefinedLinkedList = new UserDefinedLinkedList();
-        Node head = new Node(56);
+        Node head = new Node(70);
         Node middle = new Node(30);
-        Node tail = new Node(70);
+        Node tail = new Node(56);
+        
+        tail.setNext(middle);
+        middle.setNext(head);
 
-        head.setNext(middle);
-        middle.setNext(tail);
-
-        userDefinedLinkedList.head = head;
+        userDefinedLinkedList.tail = tail;
         userDefinedLinkedList.display();
     }
 }
